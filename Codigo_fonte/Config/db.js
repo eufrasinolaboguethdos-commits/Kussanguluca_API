be5ac473544c92ddd9c9adb6ad802ffcb912c878
simonjoseph <simaojose1978@gmail.com>
2026-05-14 13:52:44 +0100

    fix: adicionar porta na ligação à BD

--- Codigo_fonte/Config/db.js
+++ Codigo_fonte/Config/db.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 export const connection = await mysql.createPool({
     host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT) || 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 })
-
